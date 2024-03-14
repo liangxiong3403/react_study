a5482a704aaf2ef3fbb8690a3eca82cefd25dcb0
liangxiong <1071608617@qq.com>
2024-03-14 20:02:47 +0800

    函数组件

--- my-app/src/Contact.js
+++ my-app/src/Contact.js
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Contact extends Component {
-    render() {
-        const { contactForm } = this.props;
+const Contact = props => {
+    const { contactForm } = props;
 
-        const formList = contactForm.map(contact => {
-            return (
-                <div className="ContactForm" key={contact.id}>
-                    <div>{contact.name}</div>
-                    <div>{contact.age}</div>
-                    <div>{contact.sex}</div>
-                </div>);
-        });
-        console.log(contactForm);
-
+    const formList = contactForm.map(contact => {
         return (
-            <div className="ContactForm">
-                {formList}
-            </div>
-        );
-    }
+            <div className="ContactForm" key={contact.id}>
+                <div>{contact.name}</div>
+                <div>{contact.age}</div>
+                <div>{contact.sex}</div>
+            </div>);
+    });
+    console.log(contactForm);
+
+    return (
+        <div className="ContactForm">
+            {formList}
+        </div>
+    );
 }
 
 export default Contact;
