c973eb5555d4ed7c37b4bf4da84855f15d4ca83d
liangxiong <1071608617@qq.com>
2024-03-15 09:58:01 +0800

    数据逻辑处理

--- my-app/src/Contact.js
+++ my-app/src/Contact.js
@@ -1,15 +1,17 @@
 import React from "react";
 
+// props是形式参数(可以任意名称)
 const Contact = props => {
+    // 解构contactForm属性
     const { contactForm } = props;
 
     const formList = contactForm.map(contact => {
-        return (
+        return contact.age < 50 ? (
             <div className="ContactForm" key={contact.id}>
                 <div>{contact.name}</div>
                 <div>{contact.age}</div>
                 <div>{contact.sex}</div>
-            </div>);
+            </div>) : null;
     });
     console.log(contactForm);
 
